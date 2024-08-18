908f57d47f0d7afd4382e2594e6c7d538b489fcf
Mayller <67043747+mayller10@users.noreply.github.com>
2024-08-18 20:10:35 -0300

    Add files via upload
    Atividade de Frequência - Semana 2 - Prazo de 12 a 19 de agosto

---
+++ Aula01_React_Vianna/src/components/Ola.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const getNome = () => "Marcola";
+
+const Ola = () =>{
+    return <div>Olá, este é o primeiro código de react do {getNome()}!</div>
+}
+
+export default Ola;
---
+++ Aula01_React_Vianna/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Ola from './components/Ola'
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+);
+
+root.render(
+    <>
+    <Ola/>
+    </>   
+);
